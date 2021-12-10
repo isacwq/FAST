f69ed980ba6f592b796252490d6f74f224c8636d
Isac Tomaz da Silva <isactomaz@protonmail.com>
2021-12-10 18:49:53 -0300

    add route fastprioritize

--- py/server.py
+++ py/server.py
@@ -1,26 +1,55 @@
-from flask import Flask, request, render_template
-import lsh, competitors, fast, prioritize, scalability
 import sys
 import math
 import os
 import io
 
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+
+import prioritize
+
 template_dir = os.path.abspath('..\\templates\\')
 print(template_dir)
-app = Flask('FAST')
+app = Flask(__name__)
+Cors = CORS(app)
+CORS(app, resources={r'/*': {'origins': '*'}}, CORS_SUPPORTS_CREDENTIALS=True)
+app.config['CORS_HEADERS'] = 'Content-Type'
 print(app.template_folder)
 
-@app.route('/', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('main_form.html')
 
-@app.route('/prioritize', methods = ['POST'])
+
+@app.route('/prioritize', methods=['POST'])
 def funcionalidade_pagina():
-    prog_v =  request.form['subject']
+    prog_v = request.form['subject']
     entity = request.form['entity']
     algname = request.form['algorithm']
     repeats = int(request.form['repetitions'])
+    return fast_prioritize(prog_v, entity, algname, repeats)
 
+
+@app.route('/fastprioritize', methods=['GET', 'POST'])
+def submit_prioritize():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+
+        prog_v = post_data.get('subject')
+        entity = post_data.get('entity')
+        algname = post_data.get('algorithm')
+        repeats = post_data.get('repetitions')
+
+        print(prog_v, entity, algname, repeats)
+
+        response_object['message'] = f'{prog_v}, {entity}, {algname}, {repeats}'
+        # fast_prioritize(prog_v, entity, algname, repeats)
+    return jsonify(response_object)
+
+
+def fast_prioritize(prog_v, entity, algname, repeats):
     prog, v = prog_v.split("_")
 
     directory = "output/{}_{}/".format(prog, v)
@@ -61,7 +90,8 @@ def funcionalidade_pagina():
     output = sys.stdout.getvalue()
     sys.stdout = stdout
 
-    return output 
+    return output
+
 
 if __name__ == '__main__':
     port = 3000 if len(sys.argv) < 2 else int(sys.argv[1])
